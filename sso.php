73a6ad5eb7719ecaa4733c2e28299c978f849cd9
alttehn <31592612+alttehn@users.noreply.github.com>
2017-09-20 19:03:47 +0300

    sso

--- sso.php
+++ sso.php
@@ -16,7 +16,6 @@ if (empty($_SERVER['HTTP_HOST'])) {
 // Example url (SSO script on subdomain): "a.firstsite.com"
 // Example url (SSO script in the Drupal directory): "firstsite.com/sso.php"
 $network = array(
-   'login-dev.numberonegamer.com/sso.php',
    'bank-dev.numberonegamer.com/sso.php',
   
 );
@@ -33,7 +32,7 @@ $network = array(
 // $cookie_name_strict = true;
 
 // Validate the query parameters and network size.
-if (!sso_validate_query_params() || count($network) < 2) {
+if (!sso_validate_query_params() || count($network) < 1) {
   exit;
 }
 
